6bb24251b26e8b645b2f41ca2dc0fc9107b3d4f5
Alvaro Kú <alvaro.ku@blueoceantech.com.mx>
2023-04-12 21:48:11 -0600

    changes user entity

--- Entities/User.cs
+++ Entities/User.cs
@@ -13,6 +13,7 @@ namespace Entities
         public string PhoneNumber { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
+        public string Rol { get; set; }
         
     }
 }
