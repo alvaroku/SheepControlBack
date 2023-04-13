6991f4756066afdeaec5423282a74c6ea33a6347
Alvaro Kú <alvaro.ku@blueoceantech.com.mx>
2023-04-12 21:47:43 -0600

    DbContext

--- DataAccess/SheepControlDbContext.cs
+++ DataAccess/SheepControlDbContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,14 @@ using System.Threading.Tasks;
 
 namespace DataAccess
 {
-    public class SheepControlDbContext
+    public class SheepControlDbContext : DbContext
     {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Sheep> Sheeps { get; set; }
+        public DbSet<Vaccine> Vaccines { get; set; }
+
+        public SheepControlDbContext(DbContextOptions<SheepControlDbContext> options) : base(options)
+        {
+        }
     }
 }
