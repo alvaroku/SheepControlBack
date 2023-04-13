2b06601e9644ab16d6794ef37eaea788e87eaf53
Alvaro Kú <alvaro.ku@blueoceantech.com.mx>
2023-04-12 21:42:30 -0600

    business definition

---
+++ Business/Definitions/IUserBusiness.cs
@@ -0,0 +1,20 @@
+﻿using DataAccess.Implementations;
+using SheepControlApi.Dtos;
+using SheepControlApi.Entities.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Definitions
+{
+    public interface IUserBusiness
+    {
+        public Response<UserResponse> Create(UserRequest userRequest);
+        public IEnumerable<UserResponse> Read();
+        public Response<LoginResponse> Login(LoginRequest userRequest);
+        public Response<UserResponse> ValidarToken(ClaimsIdentity identity);
+    }
+}
