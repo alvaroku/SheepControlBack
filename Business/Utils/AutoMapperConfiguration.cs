44dbb2302300e7925589111a6742139e83346de4
Alvaro Kú <alvaro.ku@blueoceantech.com.mx>
2023-04-12 21:47:22 -0600

    mapper

---
+++ Business/Utils/AutoMapperConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Entities;
+using SheepControlApi.Entities.Dtos;
+
+namespace Business.Utils
+{
+    public class AutoMapperConfiguration
+    {
+        public static void Configure()
+        {
+            Mapper.Initialize(cfg =>
+            {
+                //mappers principales
+                cfg.CreateMap<UserRequest, User>().ReverseMap();
+
+                cfg.CreateMap<User, LoginResponse>().ReverseMap();
+
+
+            });
+        }
+    }
+}
