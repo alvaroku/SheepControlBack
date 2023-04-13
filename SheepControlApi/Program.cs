4a372ff1d08a898ba0a89d1933b4060a6821220a
Alvaro Kú <alvaro.ku@blueoceantech.com.mx>
2023-04-12 21:48:43 -0600

    bearer auth

--- SheepControlApi/Program.cs
+++ SheepControlApi/Program.cs
@@ -1,4 +1,17 @@
 
+using Business.Definitions;
+using Business.Implementations;
+using Business.Utils;
+using DataAccess;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Diagnostics.SymbolStore;
+using System.Security.Cryptography.Xml;
+using System.Text;
+
 namespace SheepControlApi
 {
     public class Program
@@ -12,7 +25,65 @@ namespace SheepControlApi
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+
+            //builder.Services.AddSwaggerGen();
+            //para boton auth
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "SheepControl", Version = "v1" });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "Jwt Auth",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[]{}
+                    }
+                });
+            });
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                    ValidAudience = builder.Configuration["Jwt:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+                };
+            });//bearer
+
+            builder.Services.AddDbContext<SheepControlDbContext>(
+                //options => options.UseInMemoryDatabase("Test")
+                options => options.UseSqlServer(builder.Configuration.GetConnectionString("ConnectionString"))
+                );//bd
+
+            builder.Services.AddScoped<IUserBusiness, UserBusiness>();//business
+
+            AutoMapperConfiguration.Configure();
+
+            // Add Cors for api access
+            /*builder.Services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
+            {
+                builder.AllowAnyOrigin()
+                       .AllowAnyMethod()
+                       .AllowAnyHeader();
+            }));*/
 
             var app = builder.Build();
 
@@ -24,6 +95,8 @@ namespace SheepControlApi
             }
 
             app.UseHttpsRedirection();
+
+            app.UseAuthentication();//bearer
 
             app.UseAuthorization();
 
